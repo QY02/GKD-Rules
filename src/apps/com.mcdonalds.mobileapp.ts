723f61273dbd28d2a0245962a0bf4c8d8a2d95a4
QY02 <1494893742@qq.com>
2026-02-07 19:29:17 +0800

    Fix bugs

--- src/apps/com.mcdonalds.mobileapp.ts
+++ src/apps/com.mcdonalds.mobileapp.ts
@@ -21,7 +21,9 @@ export default defineGkdApp({
             'View[id="HomeSplashScreen/FullScreenLoader/LottieSpinningLoaderView"]',
           ],
           action: 'none',
-          snapshotUrls: 'https://e.gkd.li/616dfb8c-9634-420f-9535-3383aa008ab9',
+          snapshotUrls: [
+            'https://e.gkd.li/616dfb8c-9634-420f-9535-3383aa008ab9',
+          ],
         },
         {
           key: 1,
@@ -32,7 +34,9 @@ export default defineGkdApp({
             'View[id="HomeSplashScreen/FullScreenLoader/LottieSpinningLoaderView"]',
           ],
           action: 'back',
-          snapshotUrls: 'https://e.gkd.li/f192ca3e-53a9-44a5-964d-a6110c2463ae',
+          snapshotUrls: [
+            'https://e.gkd.li/f192ca3e-53a9-44a5-964d-a6110c2463ae',
+          ],
         },
       ],
     },
@@ -47,7 +51,9 @@ export default defineGkdApp({
           matches: [
             'View[id="HomeSplashScreen/HomeSplashScreenContent/SplashBanner/NewsContentHero /CardImage"] < * < * < * +1 @* > View[desc="關閉"] +1 View[id="HomeSplashScreen/HomeSplashScreenContent/SplashBanner/RoundIconButtonSecondary/RoundIconButton "] -1 * < * +1 View[id="HomeSplashScreen/HomeSplashScreenContent/SplashBanner/FloatingLargePrimary/FloatingBase"] > View[desc="立即睇"]',
           ],
-          snapshotUrls: 'https://e.gkd.li/1f8fc43a-7e48-45ff-9958-aed51f579de2',
+          snapshotUrls: [
+            'https://e.gkd.li/1f8fc43a-7e48-45ff-9958-aed51f579de2',
+          ],
         },
       ],
     },
@@ -63,7 +69,9 @@ export default defineGkdApp({
           matches: [
             '@ImageView[id="com.mcdonalds.mobileapp:id/closeButton"] +1 TextView[text="請確保揀選正確的餐廳"]',
           ],
-          snapshotUrls: 'https://e.gkd.li/958d6916-934c-4652-8015-dd4235f90916',
+          snapshotUrls: [
+            'https://e.gkd.li/958d6916-934c-4652-8015-dd4235f90916',
+          ],
         },
       ],
     },
